838d15bc24ab0e25ec8096b40aa95adeea9d46c5
Enzo Toshio <enzo.toshiba@gmail.com>
2020-05-11 00:35:09 +0200

    feature: finishing implementation

--- src/example.js
+++ src/example.js
@@ -1,7 +1,29 @@
 import './assets/emails-input.css'
+import './assets/example.css'
 import EmailsInput from './emails-input'
 
-const inputContainerNode = document.querySelector('#emails-input')
-const emailsInput = new EmailsInput(inputContainerNode, {})
+const inputContainerPlaceholder = document.querySelector(
+  '#emails-input-container--placeholder'
+)
+const inputContainer = document.querySelector('#emails-input-container')
+const addEmailButton = inputContainer.querySelector('.add-email-button')
+const getEmailsCountButton = inputContainer.querySelector(
+  '.get-emails-count-button'
+)
+const emailsInput = new EmailsInput(inputContainerPlaceholder, {})
+const randomEmails = ['musafa@lion.com', 'simba@lion.com', 'nala@lion.com']
+
+addEmailButton.onclick = () => {
+  const randomIndex = Math.floor(Math.random() * randomEmails.length)
+  const randomEmail = randomEmails[randomIndex]
+  emailsInput.insertEmail(randomEmail)
+}
+
+getEmailsCountButton.onclick = () => {
+  const emails = emailsInput.getAllEmails()
+  const validEmails = emails.filter(({ isValid }) => isValid)
+
+  alert(`Valid emails: ${validEmails.length}`)
+}
 
 console.log(emailsInput)
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './assets/reset.css'
 import './assets/emails-input.css'
 import EmailsInput from './emails-input'
 
